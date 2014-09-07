24ba2ce40bebe18731f5389c96e65afb5007191f
akorol777 <akorol777@ya.ru>
2014-09-07 23:34:45 +0300

    donee

--- js/common.js
+++ js/common.js
@@ -16,14 +16,39 @@ $(document).ready(function() {
 		return false							
 	});
 
-	$('.popup').click(function(){
+	$('#for-popup2').click(function(){
+		$('#popup2').toggleClass('popup-open');
+		return false							
+	});
+
+	$('#for-popup3').click(function(){
+		$('#popup3').toggleClass('popup-open');
+		return false							
+	});
+
+	$('#for-popup-video').click(function(){
+		$('#popup-video').toggleClass('video-open');
 		return false							
 	});		
 
+	$('.popup').click(function(){
+		return false							
+	});	
+
+	$('.popup-video').click(function(){
+		return false							
+	});		
+
+	$('.popup__close').click(function(){
+		$('.popup').removeClass('popup-open');
+		$('#popup-video').removeClass('video-open');											
+	});
 
 	$(document).click(function(){
 		$('.nav').removeClass('nav-open');
-		$('#popup1').removeClass('popup-open');								
+		$('#popup1').removeClass('popup-open');	
+		$('#popup2').removeClass('popup-open');
+		$('#popup-video').removeClass('video-open');											
 	});
 
 
